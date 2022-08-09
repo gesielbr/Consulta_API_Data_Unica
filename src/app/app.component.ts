3c44d162f05ee56c19f66166ab4d3770d7c22b19
Gesiel Oliveira <gesiel.oliveira@linx.com.br>
2022-08-09 19:10:43 -0300

    DBJson, Interface, Serviço User

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'desafio_1';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+    });
+  }
 }
